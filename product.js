80dcf81a46de7af2436f03fdb1680f1f311add99
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-12-07 18:13:08 -0300

    implementacao foreach

---
+++ product.js
@@ -0,0 +1,28 @@
+const menuOptions = [
+  {
+    name: "X-Duplo Bacon",
+    price: 30,
+    vegan: false,
+    src: "./assets/x-duplo-bacon.jpg",
+  },
+  { name: "X-Bacon", price: 34, vegan: false, src: "./assets/xbacon.png" },
+  {
+    name: "X-Bacon Egg",
+    price: 39,
+    vegan: false,
+    src: "./assets/bacon-egg.png",
+  },
+  {
+    name: "Monstruoso",
+    price: 50,
+    vegan: false,
+    src: "./assets/monstruoso.png",
+  },
+  {
+    name: "Monstruoso Vegano",
+    price: 55,
+    vegan: true,
+    src: "./assets/monstruoso-vegan.png",
+  },
+  { name: "X-Vegan", price: 45, vegan: true, src: "./assets/xvegan.png" },
+];
--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const menuOptions = [
-  { name: "X-Salada", price: 30, vegan: false, src: "./assets/xsalada.jpeg" },
-  { name: "X-Bacon", price: 34, vegan: false, src: "./assets/xbacon.png" },
-  { name: "X-Bacon Egg", price: 39, vegan: false, src: "./assets/bacon-egg.png" },
-  { name: "Monstruoso", price: 50, vegan: false, src: "./assets/monstruoso.png" },
-  { name: "Big Vegano", price: 55, vegan: true, src: "./assets/xvegan.png" },
-  { name: "X-Vegan", price: 45, vegan: true, src: "./assets/xvegan.png" },
-];
+const list = document.querySelector("ul");
+list.innerHTML = "";  
+const buttonShowAll = document.querySelector(".show-all");
+
+const showAll = () => {
+  let myLi = "";
+  menuOptions.forEach((product) => {
+    myLi += `
+      <li>
+        <img src=${product.src}>
+        <p>${product.name}</p>
+        <p class="item-price">R$ ${product.price.toFixed(2)}</p>
+        <div class="vegan">${
+          product.vegan ? "Opção Vegana" : "Opção Não Vegana"
+        }</div>
+      </li>
+    `;
+  });
+
+  list.innerHTML = myLi;
+};
+
+buttonShowAll.addEventListener("click", showAll);
